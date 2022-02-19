3fb2e367e4509e529847f215df25bca5be6c28bf
Muqaddas Rashid <muqaddas@addo.ai>
2022-02-19 13:54:33 +0500

    section 2 in progress

---
+++ section2/creating-server-express.js
@@ -0,0 +1,33 @@
+//using express server
+const express = require('express')
+const app = express()
+const port = 3000;
+
+const data = require('../dev-data/data.json');
+
+app.get('/', (req, res) => {
+  res.send('Hello this is homepage');
+});
+
+app.post('/overview', (req, res) => {
+  res.send('This is an overview page');
+});
+
+app.get('/about', (req, res) => {
+  res.send('This is about page');
+});
+
+app.get('/api', (req, res) => {
+  res.set('Content-Type', 'application/json');
+  res.send((data));
+})
+
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res){
+  res.set('Content-Type', 'text/html');
+  res.send('<h1>Page not Found</h1>', 404);
+});
+
+app.listen(port, () => {
+  console.log(`listening to http://localhost:3000`)
+})
---
+++ section2/creating-server-http.js
@@ -0,0 +1,32 @@
+// creting server using http package
+const http = require("http");
+const url = require("url");
+const path = require("path");
+const fs = require("fs");
+
+//reading json data
+const data = fs.readFileSync( path.join(__dirname, "../dev-data/data.json"), "utf8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  //Adding routes to server
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is an overview page");
+  } else if (pathName === "/product") {
+    res.end("This is a product page");
+  } else if (pathName === "/api") {2  
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+    });
+    res.end("<h1>404 Not Found</h1>");
+  }
+});
+server.listen(8000, (err, res) => {
+  console.log("listening to http://localhost:8000");
+});
